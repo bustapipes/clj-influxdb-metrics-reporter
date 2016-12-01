b986b5081345ec5c77a0836fb60313c9a6d35055
Busta Pipes <bustapipes@gmail.com>
2016-12-01 11:14:25 -0700

    Version 0.0.3

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
 ;
 ; You should have received a copy of the GNU General Public License
 ; along with clj-influxdb-metrics-reporter  If not, see <http://www.gnu.org/licenses/>.
-(defproject com.github.bustapipes/clj-influxdb-metrics-reporter "0.0.3-SNAPSHOT"
+(defproject com.github.bustapipes/clj-influxdb-metrics-reporter "0.0.3"
   :description "Report metrics to Influx DB"
   :url "https://github.com/bustapipes/clj-influxdb-metrics-reporter"
   :license {:name "Lesser General Public License"
